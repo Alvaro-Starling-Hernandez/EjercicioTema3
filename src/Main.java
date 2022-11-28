90b2b3857eb11e5c6d4a6b882194873fd7018db6
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-11-28 00:07:36 -0400

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        sumar(10,5,9);
+        Coche miCoche = new Coche();
+        miCoche.sumarPuertas();
+        System.out.println(miCoche.numeroPuertas);
+    }
+
+    public static void sumar(int a,int b, int c){
+        System.out.println(a+b+c);
+    }
+}
+
+class Coche {
+    int numeroPuertas = 0;
+
+    public void sumarPuertas(){
+        this.numeroPuertas++;
     }
 }
